c7db965294743c3f619e15923510e7f3a475777c
siloni <orrs@namogoo.com>
2016-11-02 16:42:06 +0200

    Add support for phrase param in pack method (see in github orrsiloni/packer)

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var es = require('event-stream')
-  , packer = require('packer')
-  , gutil = require('gulp-util')
-  , Buffer = require('buffer').Buffer
-  , path = require('path')
+var es = require('event-stream'),
+    packer = require('packer'),
+    gutil = require('gulp-util'),
+    Buffer = require('buffer').Buffer;
 
 module.exports = function (opt) {
-  opt = opt || {}
-  opt.base62 = opt.base62 || false
-  opt.shrink = opt.shrink || false
+  opt = opt || {};
+  opt.base62 = opt.base62 || false;
+  opt.shrink = opt.shrink || false;
+  opt.phrase = opt.phrase || false;
 
   function minifyFile(file) {
-    if (file.isNull()) return this.emit('data', file) // pass along
-    if (file.isStream()) return this.emit('error', new Error('gulp-packer: Streaming not supported'))
+    if (file.isNull()) return this.emit('data', file); // pass along
+    if (file.isStream()) return this.emit('error', new Error('gulp-packer: Streaming not supported'));
 
-    var str = file.contents.toString('utf8')
-      , dest = gutil.replaceExtension(file.path, '.min.js')
-      , data = packer.pack(str, opt.base62, opt.shrink)
+    var str = file.contents.toString('utf8'),
+        dest = gutil.replaceExtension(file.path, '.min.js'),
+        data = packer.pack(str, opt.base62, opt.shrink, opt.phrase);
 
-    file.path = dest
-    file.contents = new Buffer(data)
+    file.path = dest;
+    file.contents = new Buffer(data);
 
-    this.emit('data', file)
+    this.emit('data', file);
   }
 
   return es.through(minifyFile)
-}
+};
